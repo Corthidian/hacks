2e309b055076a5526b042c3b5c6e7e51d9baac4a
Corthidian <lucifer880799@gmail.com>
2023-10-22 17:50:51 +0500

    fixing errors

--- distancecalculation.c
+++ distancecalculation.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #define EARTH_RADIUS 6371.0
-double to radians (double degrees)
-{
-    return degrees * M_PI/180.0;
+double toRadian (double degrees)
+ {
+    
+return degrees * M_PI / 180.0;
 }
 double computeDistance (double lat1,double lon1,double lat2,double lon2)
 {
@@ -14,7 +15,7 @@ double computeDistance (double lat1,double lon1,double lat2,double lon2)
     lon2 = toRadian(lon2);
     double dlon = lon2 - lon1;
     
-    return EARTH_RADIUS * (acos sin(lat1) *sin(lat2) + cos(lat2) * cos(lat1) * cos(dlon));
+    return EARTH_RADIUS * (acos (sin(lat1) *sin(lat2) + cos(lat2) * cos(lat1) * cos(dlon)));
 }
  int main ()
  {
