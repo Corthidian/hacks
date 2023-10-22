560c95548e6968deb58d8c336ffffe21a7a3a306
Corthidian <lucifer880799@gmail.com>
2023-10-22 17:42:53 +0500

    printing out the final result

--- distancecalculation.c
+++ distancecalculation.c
@@ -28,9 +28,11 @@ double computeDistance (double lat1,double lon1,double lat2,double lon2)
     printf("Enter the latittude of destination: ");
     scanf("%lf",&lon2);
     double distance = computeDistance(lat1,lat2,lon1,lon2);
-    
-
-
+    printf("\nLocation Distance\n");
+    printf("========================\n");
+    printf("Origin:  (%.6f, %.6f)\n",lat2,lon2);
+    printf("Air distance is %.6f\n",distance);
+    return 0;
  }
  
 
